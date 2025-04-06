922c163fad167ce4712c8608d909c5ca788e2dcc
shguijj <jiajia_yuwei@163.com>
2025-04-06 17:01:18 +0800

    fix validation

--- backend/sugar/src/main/java/evliess/io/config/CustomFilter.java
+++ backend/sugar/src/main/java/evliess/io/config/CustomFilter.java
@@ -1,6 +1,5 @@
 package evliess.io.config;
 
-import evliess.io.utils.TokenUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,13 +29,6 @@ public class CustomFilter extends OncePerRequestFilter {
         }
         String token = request.getHeader(Constants.X_TOKEN);
         String openid = request.getHeader(Constants.X_OPENID);
-        // Check if the openid is null or empty
-        if (!TokenUtils.isValidOpenid(openid)) {
-            Authentication authentication = new UsernamePasswordAuthenticationToken("", "");
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            filterChain.doFilter(request, response);
-            return;
-        }
         Authentication authentication = new UsernamePasswordAuthenticationToken(openid, token);
         Authentication verifiedAuth = this.authenticationManager.authenticate(authentication);
         SecurityContextHolder.getContext().setAuthentication(verifiedAuth);
